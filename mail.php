4ecf03625cb85a68f843a884e53b411d9cc67794
Jose Ibanez <jibanez.romany74@gmail.com>
2018-01-09 15:37:51 -0400

    website is now in html

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+
+function has_header_injection($str) {
+  return preg_match( "/[\r\n]/", $str );
+}
+
+if (isset($_POST["name"]) && isset($_POST["message"]) && isset($_POST["email"])) {
+  $name = trim(htmlspecialchars($_POST['name']));
+  $email = trim(htmlspecialchars($_POST['email']));
+  $msg = htmlspecialchars($_POST['message']);
+
+  if (
+    has_header_injection($name) ||
+    has_header_injection($email)
+  ) {
+    die ();
+  }
+
+  $toJose = "jose@blindpenguincode.com";
+
+  $subject = $name.'with email'.$email.'sent you a message';
+
+  $message = "Nombre: $name, Email: $email, Mensaje: $msg";
+  
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "From: " . $name . " <" . $email . ">\r\n";
+  $headers .= "X-Priority: 1\r\n";
+  $headers .= "X-MSMail-Priority: High\r\n\r\n";
+
+  $message = wordwrap($message, 72);
+
+  mail($toJose, $subject, $message, $headers);
+  mail($toMiguel, $subject, $message, $headers);
+}
+?>
